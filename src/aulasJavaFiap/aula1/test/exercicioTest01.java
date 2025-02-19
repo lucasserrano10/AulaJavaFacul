4abe3c82a644989d7f88aad3896b04ab1b7cd32a
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2025-02-18 21:40:08 -0300

    aula01

---
+++ src/aulasJavaFiap/aula1/test/exercicioTest01.java
@@ -0,0 +1,21 @@
+package aulasJavaFiap.aula1.test;
+
+public class exercicioTest01 {
+    public static void main(String[] args) {
+        // Exercicio 1
+
+        Float num1 = 1.5F;
+        Float num2 = 3.5F;
+        Float num3 = 56.5F;
+        Float media = (num1+num2+num3)/3;
+        System.out.println(media);
+
+        // Cast
+        //Em Java, cast é o processo de conversão de um tipo de dado para outro
+        double numeroDecimal = 10.5;
+        int numeroInteiro = (int)numeroDecimal;
+        System.out.println(numeroInteiro);
+
+
+    }
+}
